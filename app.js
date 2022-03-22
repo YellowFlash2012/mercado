1ed366e140e64880b6e74a4eae35452a17262466
YellowFlash2012 <cyclop9@hotmail.com>
2022-03-22 13:37:13 +0100

    added the toast option and managed to stack the product-img atop one another

--- app.js
+++ app.js
@@ -17,3 +17,14 @@ plusQty.addEventListener("click", () => {
     console.log("clicked +1");
     return qty.textContent ++;
 })
+
+// *****toast config******
+var toastTrigger = document.getElementById("liveToastBtn");
+var toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+    toastTrigger.addEventListener("click", function () {
+        var toast = new bootstrap.Toast(toastLiveExample);
+
+        toast.show();
+    });
+}
